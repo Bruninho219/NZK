952f1f6fc0b6c947b6bc29a25877368317892b2f
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2026-07-30 21:33:03 -0300

    .

--- js/config.js
+++ js/config.js
@@ -1,13 +1,8 @@
 // js/config.js
 // Inicializa o cliente globalmente
-// const sb = supabase.createClient(URL, KEY);
 
 var CONFIG = {
     URL: "https://moyakvhrgvllsjznohjm.supabase.co",
-<<<<<<< HEAD
     KEY: "sb_publishable_CUh67eIGh9qX2QiMtX3zuA_TRgj23BG",
 	YOUTUBE_API_KEY: "AIzaSyBn6tCewgOy7mjeeA5LEDqZ6BUElvDplfc"
-=======
-    KEY: "sb_publishable_CUh67eIGh9qX2QiMtX3zuA_TRgj23BG"
->>>>>>> 3a768eb615762122901d272dab4faf71cd267a72
 };
