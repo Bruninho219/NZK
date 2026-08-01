7af543c37f9c56f6a13e9d4431380906101c9356
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2026-08-01 16:30:49 -0300

    .

--- start2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Executador NZK Bot - Reinicio Automatico
-
-:loop
-echo [%time%] Iniciando o NZK Bot...
-python main.py
-echo [%time%] ⚠️ O bot foi finalizado ou interrompido. Reiniciando em 3 segundos...
-timeout /t 3 /nobreak >nul
-goto loop
