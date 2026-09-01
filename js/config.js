33e0d58d7b409508b07a412710c6ec889298e780
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2026-08-31 21:27:20 -0300

    .

--- js/config.js
+++ js/config.js
@@ -6,4 +6,4 @@ var CONFIG = {
 	YOUTUBE_API_KEY: "AIzaSyBn6tCewgOy7mjeeA5LEDqZ6BUElvDplfc"
 };
 
-window.NZK_VERSION = "0.10.11.19";
+window.NZK_VERSION = "0.10.12.0";
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION = "0.10.11.19"
+VERSION = "0.10.12.0"
 BOT_NAME = "NZK"
 DESCRIPTION = "Leia mais em www.nzk.app.br"
 AUTHOR = "bruninho219"
